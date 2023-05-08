c0265d2171524a9bf8ff7e50d6465ffac8706541
Touathi Vang <Touathi@outlook.com>
2023-05-08 14:19:38 -0500

    could not get addClass to work so i did style instead.

--- script.js
+++ script.js
@@ -11,18 +11,28 @@ let clickNum = 1
 function clickBtn() {
 
 
+    
+
     // Append a div and inside the div have a <p> that is able to change number every
     // time the generate button is clicked.
-    $('#divGoHere').append(`
-    <div>
+    $('body').append(`
+    <div id = "needGoRed" style = "background-color: red;">
 
         <button id ="yellowBtn">Yellow</button>
         <button id ="DeleteBtn">Delete</button>
         <p class = "numHere"> <span id="clickNum">${clickNum}</span>
     </div>
 
-    `)
-    
-    
+    `);
+
+    // Could not get this to work
+    // $('#needGoRed').addClass("backRed");
+    // $('#needGoRed').css("background-color", "red")
+
     clickNum++
+
+}
+
+function goYellow() {
+    
 }
